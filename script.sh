7344f6026ae72aa59066237d0ebeb9de20485e7a
kickman2l <geraskopavel@gmail.com>
2017-03-01 16:59:10 +0200

    qweqwe

--- script.sh
+++ script.sh
@@ -5,8 +5,7 @@ dir_old="/opt/deployment/old"
 dir_cur="/opt/deployment/curr"
 dir_dist="/opt/deployment/dist"
 
-echo $VER_ART
-echo ${VER_ART}
+echo $1
 
 
 curl -u admin:admin123 -o /opt/${VER_ART}-index.tar.gz http://nexus/repository/deplyment-jboss/$VER_ART-index.tar.gz 
