6747da2399536db34282717008f2d9bff448bda2
kickman2l <geraskopavel@gmail.com>
2017-03-01 12:01:48 +0200

    asdasd

---
+++ script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+dir="/opt/deployment"
+dir_old="/opt/deployment/old"
+dir_cur="/opt/deployment/curr"
+dir_dist="/opt/deployment/dist"
+
+curl -u admin:admin123 -O http://nexus/repository/deplyment-jboss/12-index.tar.gz
+echo $VER_ART
+
+
+
+if [[ ! -e $dir ]]; then
+    mkdir $dir
+fi
+
+if [[ ! -e $dir_old ]]; then
+    mkdir $dir_old
+fi
+
+if [[ ! -e $dir_cur ]]; then
+    mkdir $dir_cur
+fi
+
+if [[ ! -e $dir_dist ]]; then
+    mkdir $dir_dist
+else
+    rm -f $dir_dist/*
+fi
